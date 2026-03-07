0298d584234bcbc245ce587e8859bee6b6425d68
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2026-03-07 16:57:26 +0530

    Add files via upload

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -38,7 +38,6 @@ export const deleteTransaction = (id) => api.delete(`/transactions/${id}`);
 export const getDebts = () => api.get('/debts/');
 export const createDebt = (data) => api.post('/debts/', data);
 export const updateDebt = (id, data) => api.put(`/debts/${id}`, data);
-export const payDebt = (id, data) => api.post(`/debts/${id}/pay`, data);
 export const deleteDebt = (id) => api.delete(`/debts/${id}`);
 export const splitBill = (data) => api.post('/debts/split', data);
 
